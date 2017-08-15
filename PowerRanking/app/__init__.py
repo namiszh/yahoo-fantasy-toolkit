a1e7734642378ce484422b494f72b3093185fc66
huang <shaozuo.huang@siemens.com>
2017-08-15 16:32:31 +0800

    make the web page navigator work

--- PowerRanking/app/__init__.py
+++ PowerRanking/app/__init__.py
@@ -12,4 +12,19 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+class Status():
+
+    def __init__(self, current_league = 0, current_team = 0, current_week = 0, max_week=25, type=0):
+        self.current_league = current_league
+        self.current_team = current_team
+        self.current_week = current_week
+        self.max_week = max_week
+
+        # type = 0: show data by week
+        # type = 1: show data by team
+        self.type = type
+
+status = Status()
+
 from app import views, models
+
--- PowerRanking/app/views.py
+++ PowerRanking/app/views.py
@@ -5,37 +5,42 @@
     :copyright: (c) 2017 by Marvin Huang
 """
 
-from flask import render_template, redirect, url_for
-from app import app
+from flask import render_template, redirect, url_for, request
+import pandas as pd
+from bokeh.charts import Histogram
+from bokeh.embed import components
+from app import app, status
 
-CURRENT_LEAGUE_ID = 1
-CURRENT_WEEK = 1
-
-curLeagueId=1
-curWeek=1
-curTeam=1
 
 @app.route('/')
 @app.route('/index')
-def show_index():
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html', user=user)
+def index():
+    # goto current league
+    url = url_for('league', league_id=status.current_league)
+    return redirect(url)
+
+@app.route('/type=<int:type_id>')
+def type(type_id):
+    status.type = type_id
+    url = url_for('league', league_id=status.current_league)
+    return redirect(url)
 
 
-# @app.route('/league=<league_id>')
-# def show_league(league_id):
+@app.route('/league=<int:league_id>')
+def league(league_id):
 
-#     # if CURRENT_LEAGUE_ID != league_id:
-#         # CURRENT_LEAGUE_ID = league_id
-#         # CURRENT_WEEK = 0    # show total
+    status.current_league = league_id
+    if status.type == 0:
+        url = url_for('week', league_id=league_id, week=status.current_week)
+    else:
+        url = url_for('team', league_id=league_id, team_id=status.current_team)
 
-#     url = url_for('show_week', league_id=league_id, week=0)
-#     print(url)
-#     return redirect(url)
+    return redirect(url)
 
 
-@app.route('/league=<league_id>/team=<team_id>')
-def show_team(league_id, team_id):
+
+@app.route('/league=<int:league_id>/team=<int:team_id>')
+def team(league_id, team_id):
     '''
     Show trend chart of a team.
     
@@ -44,13 +49,14 @@ def show_team(league_id, team_id):
 
     each category (PTS, AST...) has a curve
     '''
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html', menu_type="team")
+    status.current_league = league_id
+    status.type = 1
+    status.current_team = team_id
+    return render_template('index.html', status=status)
 
-# @app.route('/', defaults={'league_id' :curLeagueId, 'week': curWeek})
-# @app.route('/league=<league_id>', defaults={'week': 1})
-@app.route('/league=<league_id>/week=<week>')
-def show_week(league_id, week):
+
+@app.route('/league=<int:league_id>/week=<int:week>')
+def week(league_id, week):
     '''
     Show power ranking bar graph of the week.
 
@@ -59,34 +65,11 @@ def show_week(league_id, week):
 
     week=0 means the season total
     '''
-    print(week)
-    user = {'nickname': 'Miguel'}  # fake user
-    return render_template('index.html', menu_type="week")
+    status.current_league = league_id
+    status.type = 0
+    if int(week) > status.max_week:
+        week = status.max_week
+    status.current_week = week
+    return render_template('index.html', status=status)
 
-@app.route("/chart")
-def chart():
-    labels = [
-        "B3-Jordan",
-        "B1-pippo",
-        "A5-Gray Potato",
-        "A3-Marsmnky",
-        "C5-unbe",
-        "B4-????????",
-        "xiuxian",
-        "C3-??????",
-        "D5 - ????",
-        "C4-lebronjames",
-        "C1-Lydia",
-        "D4-????",
-        "10,000.00",
-        "A2-??????????",
-        "A4-??????",
-        "C2-Stephen",
-        "D3 - ?U?U",
-        "D1-????",
-        "B2-Sin",
-        "B5-dragonball"
-    ]
 
-    values = [104.5, 158, 70, 139, 126.5, 145.5, 80, 117, 113, 127.5, 52.5, 113.5, 161, 128, 103, 90.5, 126, 138.5, 126.5, 89.5]
-    return render_template('barchart.html', values=values, labels=labels)

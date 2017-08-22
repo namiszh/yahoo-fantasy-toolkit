6c31317b546d067ab5b8c264d68b43ea42548144
huang <shaozuo.huang@siemens.com>
2017-08-22 23:55:01 +0800

    Use explict wait to replace time sleep

--- Lineup/README.md
+++ Lineup/README.md
@@ -1,10 +1,21 @@
 # Rob to start active players
 
-Python Selenium script that logs into your Yahoo fantasy basketball account and starts active players for today and upcoming days. Accepts arguments username, password, the number of days you would like the bot to process into the future and whether you want to see what the browser is running (useful for debugging) or run in headless mode.
+Python Selenium script that logs into your **Yahoo Fantasy Basketball** account, and starts active players for **All YOUR TEAMS** for today and upcoming days. Accepts arguments username, password, the number of days you would like the bot to process into the future and whether you want to see what the browser is running (useful for debugging) or run in headless mode.
+
+## How To Run
 
 `python start-active-players.py`
 
 You can include these details as options to avoid having to fill them in each time. For example to start active players for the next week including today.
 
-`python start-active-players.py --days=7 --username=YahooUsername --password=Y0urYah00Passw0rd --headless=False`
+`python start-active-players.py --u=YahooUsername --p=Y0urYah00Passw0rd --d=7 --h=True`
+
+
+## Dependencies
+
+* [Python](https://www.python.org/)
+* [Click](http://click.pocoo.org/)
+* [Selenium WebDriver](http://www.seleniumhq.org/projects/webdriver/)
+* [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/downloads)
+* [PhantomJS](http://phantomjs.org/)
 
--- Lineup/start-active-players.py
+++ Lineup/start-active-players.py
@@ -1,25 +1,23 @@
 #!/usr/bin/python
 
-import time
 from selenium import webdriver
 from selenium.webdriver import PhantomJS
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.chrome.options import Options
-# from selenium.webdriver.common.action_chains import ActionChains
+from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
-# from sys import argv
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 import click
 
 @click.command()
-@click.option('--username', prompt='Your Yahoo username', help='Your Yahoo account username')
-@click.option('--password', prompt='Your Yahoo password', help='Your Yahoo account password')
-@click.option('--days', type=int, default=7, prompt='Number of days to set active lineup', help='Number of days to set active lineup')
-@click.option('--headless', type=bool, default=True, prompt='Do you want to run in headless mode? [True|False]', help='If True you won\'t see what\'s going on while it\'s running. If false you will see the browser render the steps.')
-def start_active_players(username, password, days, headless):
+@click.option('--u', prompt='Your Yahoo username', help='Your Yahoo account username')
+@click.option('--p', hide_input=True, prompt='Your Yahoo password', help='Your Yahoo account password')
+@click.option('--d', type=int, default=7, prompt='Number of days to set active lineup', help='Number of days to set active lineup')
+@click.option('--h', type=bool, default=True, prompt='Do you want to run in headless mode? [True|False]', help='If True you won\'t see what\'s going on while it\'s running. If false you will see the browser render the steps.')
+def start_active_players(u, p, d, h):
 	"""Simple python program that sets your active players for the next number DAYS."""
-	print("Logging in as: " + username)
+	print("Logging in as: " + u)
 
-	if(headless):
+	if(h):
 		DesiredCapabilities.PHANTOMJS['phantomjs.page.settings.userAgent'] = 'Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:16.0) Gecko/20121026 Firefox/16.0'
 		driver = webdriver.PhantomJS()
 	else:
@@ -28,45 +26,44 @@ def start_active_players(username, password, days, headless):
 		driver.set_window_size(1920, 1080)
 		driver.maximize_window()
 
+	# login into yahoo
 	driver.get('https://login.yahoo.com/?.src=fantasy&specId=usernameRegWithName&.intl=us&.lang=en-US&authMechanism=primary&yid=&done=https%3A%2F%2Fbasketball.fantasysports.yahoo.com%2Fnba%2F%3Futmpdku%3D1&eid=100&add=1')
+	delay = 8 # seconds
 
-	driver.find_element_by_id('login-username').send_keys(username)
+	WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'login-username'))).send_keys(u)
 	driver.find_element_by_id('login-signin').click()
-	time.sleep(8)
-	driver.find_element_by_id('login-passwd').send_keys(password)
+
+	WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, 'login-passwd'))).send_keys(p)
 	driver.find_element_by_id('login-signin').click()
-	time.sleep(8)
 
-	# hover to Fantasy Basketball to display the hidden dropdown menu 
-	# teams = driver.find_element_by_xpath("//li[@class = 'Navitem Navitem-main Navitem-fantasy Va-top Fl-start Topstart']")
-	# hov = ActionChains(driver).move_to_element(teams)
-	# hov.perform()
-	# time.sleep(1)
+	# make sure the 'My Teams and Leagues' Table is loaded
+	WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "gamehome-teams")))
 
-	# print("current url", driver.current_url)
+	# find all leagues and teams
 	leagues = driver.find_elements_by_xpath("//div[@class='Grid-table']//dd[@class='Grid-u D-i']//a[@class='F-reset']")
-	league_names = [league.text for league in leagues]
 	teams = driver.find_elements_by_xpath("//div[@class='Grid-table']//a[@class='Block Fz-sm Phone-fz-xs Pbot-xs']")
+	league_names = [league.text for league in leagues]
 	team_urls = [team.get_attribute("href") for team in teams]
 	team_names = [team.text for team in teams]
 
 	for leangue_name, team_name, team_url in zip(league_names, team_names, team_urls):
-		print("Starting active players league='{}', team='{}'".format(leangue_name, team_name))
+		print("Starting active players for: league='{}', team='{}'".format(leangue_name, team_name))
 		driver.get(team_url)
 
-		time.sleep(2)
-
-		for x in range(0, days):
-			date_text = driver.find_element_by_xpath("//span[@id='selectlist_nav']//a[@href='#']//span[@class='flyout-title']").text
+		# starting players for days.
+		for x in range(0, d):
+			date_text = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//span[@id='selectlist_nav']//a[@href='#']//span[@class='flyout-title']"))).text
 			print("Starting active players for: " + date_text)
 
 		# 	driver.find_element_by_xpath("//a[text() = 'Start Active Players']").click()
 		# 	time.sleep(2)
 
-			driver.find_element_by_xpath("//span[@id='selectlist_nav']//a[contains(@class, 'Js-next')]").click()
-			time.sleep(2)
+			# click 'next day'
+			WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//span[@id='selectlist_nav']//a[contains(@class, 'Js-next')]"))).click()
 
 	driver.quit()
+
+	print("Starting active players Finished.")
 
 if __name__ == '__main__':
 	start_active_players()

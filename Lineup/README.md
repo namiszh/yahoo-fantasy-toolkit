f5ec1384ae1433c5842c17b6a4bdb8ec3530b2fb
huang <shaozuo.huang@siemens.com>
2017-09-27 16:21:57 +0800

    make start active players script work

--- Lineup/README.md
+++ Lineup/README.md
@@ -1,15 +1,21 @@
-# Rob to start active players
+# Start Active Players
 
-Python Selenium script that logs into your **Yahoo Fantasy Basketball** account, and starts active players for **All YOUR TEAMS** for today and upcoming days. Accepts arguments username, password, the number of days you would like the bot to process into the future and whether you want to see what the browser is running (useful for debugging) or run in headless mode.
+Python Selenium script that logs into your **Yahoo Fantasy Basketball** account, and starts active players for your teams for today and upcoming days. 
 
 ## How To Run
 
 `python start-active-players.py`
 
-You can include these details as options to avoid having to fill them in each time. For example to start active players for the next week including today.
+You will be prompted to input information such as your yahoo user name, password, league ids, the number of days you would like to process. You can include these details as options to avoid having to fill them in each time. For example: to start active players for the next week including today for all your leagues.
 
-`python start-active-players.py --u=YahooUsername --p=Y0urYah00Passw0rd --d=7 --h=True`
+`python start-active-players.py --u=YahooUsername --p=Y0urYah00Passw0rd --l=all --d=7 --h=False`
 
+If you only want to start active players for some of your leagues, you can explicitly specify the leagues ids with the --l option.
+If multiple leagues, separate them with space.
+
+`python start-active-players.py --u=YahooUsername --p=Y0urYah00Passw0rd --l=12345 45678 --d=7 --h=False`
+
+The option '--h' specifies whether your want to see what the browser is running (useful for debugging) or run in headless mode.
 
 ## Dependencies
 
@@ -18,4 +24,6 @@ You can include these details as options to avoid having to fill them in each ti
 * [Selenium WebDriver](http://www.seleniumhq.org/projects/webdriver/)
 * [ChromeDriver](https://sites.google.com/a/chromium.org/chromedriver/downloads)
 * [PhantomJS](http://phantomjs.org/)
+* [BeautifulSoup](https://pypi.python.org/pypi/beautifulsoup4/)
+
 
--- Lineup/start-active-players.py
+++ Lineup/start-active-players.py
@@ -1,19 +1,80 @@
 #!/usr/bin/python
 
+from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver import PhantomJS
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.expected_conditions import _find_element
 from selenium.webdriver.support.ui import WebDriverWait
 import click
+import re
+
+
+class element_text_changed(object):
+    """
+    class to determine whether the text of a web element has changed.
+    """
+    def __init__(self, locator, text):
+        self.locator = locator
+        self.text = text
+
+    def __call__(self, driver):
+        actual_text = _find_element(driver, self.locator).text
+        return actual_text != self.text
+
+# class element_id_changed(object):
+#     """
+#     class to determine whether the id of a web element has changed.
+#     """
+#     def __init__(self, locator, old_id):
+#         self.locator = locator
+#         self.old_id = old_id
+
+#     def __call__(self, driver):
+#         new_id = _find_element(driver, self.locator).get_attribute("id")
+#         print(self.old_id, new_id)
+#         return new_id != self.old_id
+
+class url_changes(object):
+    """An expectation for checking the current url.
+    url is the expected url, which must not be an exact match
+    returns True if the url is different, false otherwise."""
+    def __init__(self, url):
+        self.url = url
+
+    def __call__(self, driver):
+        return self.url != driver.current_url
+
+def output_alternates(page):
+    """
+    Output information if there are players on bench who are available to play.
+    """
+    soup = BeautifulSoup(page, "html.parser")
+    try:
+        bench = soup.find_all('tr', class_='bench')
+        bench_bios = [p.find('div', class_='ysf-player-name') for p in bench]
+        opponents = [p.find_all('td', recursive=False)[3].text for p in bench]
+
+        for player, opponent in zip(bench_bios, opponents):
+        	n = player.find('a', class_='name')
+        	d = player.find('span')
+        	if n and d:
+	        	if lopponent:
+	        		name = n.text
+	        		pos = d.text
+	        		print('    - Alternate: %s (%s) [%s]' % (name, pos, opponent))
+    except:
+        pass
 
 @click.command()
 @click.option('--u', prompt='Your Yahoo username', help='Your Yahoo account username')
 @click.option('--p', hide_input=True, prompt='Your Yahoo password', help='Your Yahoo account password')
+@click.option('--l', default='all', prompt='Your teams (all or league ids separated by space)', help='Your Yahoo Teams')
 @click.option('--d', type=int, default=7, prompt='Number of days to set active lineup', help='Number of days to set active lineup')
 @click.option('--h', type=bool, default=True, prompt='Do you want to run in headless mode? [True|False]', help='If True you won\'t see what\'s going on while it\'s running. If false you will see the browser render the steps.')
-def start_active_players(u, p, d, h):
+def start_active_players(u, p, l, d, h):
 	"""Simple python program that sets your active players for the next number DAYS."""
 	print("Logging in as: " + u)
 
@@ -40,30 +101,60 @@ def start_active_players(u, p, d, h):
 	WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "gamehome-teams")))
 
 	# find all leagues and teams
-	leagues = driver.find_elements_by_xpath("//div[@class='Grid-table']//dd[@class='Grid-u D-i']//a[@class='F-reset']")
-	teams = driver.find_elements_by_xpath("//div[@class='Grid-table']//a[@class='Block Fz-sm Phone-fz-xs Pbot-xs']")
+	leagues = driver.find_elements_by_xpath("//div[@id='gamehome-teams']//h3//a")
 	league_names = [league.text for league in leagues]
-	team_urls = [team.get_attribute("href") for team in teams]
-	team_names = [team.text for team in teams]
+	print("You have", len(leagues), "leagues:", league_names)
+	league_urls = [league.get_attribute("href") for league in leagues]
+	league_ids = [re.search(r'/nba/(\d+)$', url).group(1) for url in league_urls]
 
-	for leangue_name, team_name, team_url in zip(league_names, team_names, team_urls):
-		print("Starting active players for: league='{}', team='{}'".format(leangue_name, team_name))
-		driver.get(team_url)
+	process_team_list = dict()
+	if l=='all':
+		for league_name, league_url in zip(league_names, league_urls):
+			process_team_list[league_name] = league_url
+	else:
+		leagues = l.split()
+		for league_name, lid, league_url in zip(league_names, league_ids, league_urls):
+			if lid in leagues:
+				process_team_list[league_name] = league_url
+
+		if not process_team_list :
+			print("Cannot not find leagues with id", leagues)
+			return
+
+	for league_name, league_url in process_team_list.items():
+		print("Starting active players for league '{}'".format(league_name))
+		driver.get(league_url)
+		WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "sitenav")))
+
+		driver.find_element_by_link_text("My Team").click()
+		WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.ID, "full_stat_nav")))
 
 		# starting players for days.
 		for x in range(0, d):
 			date_text = WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//span[@id='selectlist_nav']//a[@href='#']//span[@class='flyout-title']"))).text
 			print("Starting active players for: " + date_text)
 
-		# 	driver.find_element_by_xpath("//a[text() = 'Start Active Players']").click()
-		# 	time.sleep(2)
+			current_url = driver.current_url
+			driver.find_element_by_link_text("Start Active Players").click()
+			WebDriverWait(driver, delay).until(url_changes(current_url))
+			
+			# if there are bench player that has match, output info
+			output_alternates(driver.page_source)
 
 			# click 'next day'
 			WebDriverWait(driver, delay).until(EC.presence_of_element_located((By.XPATH, "//span[@id='selectlist_nav']//a[contains(@class, 'Js-next')]"))).click()
 
+			# wait until page refreshes, that is the date has changed.
+			WebDriverWait(driver, delay).until(
+			    element_text_changed(
+			        (By.XPATH, "//span[@id='selectlist_nav']//a[@href='#']//span[@class='flyout-title']"), date_text ) )
+
+
 	driver.quit()
 
 	print("Starting active players Finished.")
+
+
 
 if __name__ == '__main__':
 	start_active_players()

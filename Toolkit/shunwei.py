915b2fcbde0bc521b5d11caeb2bea184df7c0c7b
Huang, Shaozuo (Marvin) <shaozuo.huang@siemens.com>
2018-09-16 12:10:02 +0800

    use txt file as input

--- Toolkit/shunwei.py
+++ Toolkit/shunwei.py
@@ -14,27 +14,27 @@ import csv
 import os
 import pandas as pd
 
-def output_to_csv(file_name, names, orders) :
+def output(file_name, names, orders) :
 
-    print('Write to result to file "{}"'.format(file_name))
-    with open(file_name, 'w', newline='') as csvfile:
-        fieldnames = ['名单', '顺位']
-        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-        writer.writeheader()
+    print('============== 抽签结果 ============== ')
+    with open(file_name, 'w') as file:
 
         index = 1;
         while index <= len(names):
             for name, order in zip(names, orders):
                 if order == index:
-                    writer.writerow({'名单': name, '顺位': order})
+                    line = "{}\t{}".format(order, name)
+                    print(line)
+                    file.write(line + "\n") 
                     index+=1
                     break
+    print('>>>>>>>> 已保存到 "{}"'.format(file_name))
 
 def randomName(initial_names):
 
     names = []
 
-    print('打乱后的名单：')
+    print('============ 打乱后的名单 ============ ')
     while len(initial_names) > 1:
         num = randint(0, len(initial_names)-1)
         tmp_name = initial_names[num]
@@ -50,7 +50,7 @@ def randomName(initial_names):
 def randomOrder(count):
     orders = []
 
-    print('打乱后的顺位：')
+    print('============ 打乱后的顺位 ============ ')
     while len(orders) < count:
         num = randint(1, count)
         if orders.count(num) == 0:
@@ -61,13 +61,16 @@ def randomOrder(count):
 
 @click.command()
 @click.option('--t', type=int, default=3, prompt='你想做什么？（1：生成最后结果；2：打乱名单；3：打乱顺位)')
-@click.option('--f', prompt='初始名单csv文件', help='文件格式为（txt|csv）')
+@click.option('--f', prompt='请提供初始名单文件', help='文件格式为txt）')
 def main(t, f):
-    # read initial name from csv file.
-    df = pd.read_csv(f, encoding = "ISO-8859-1")
-    initial_names = df[df.columns[0]].tolist()
-    print('初始名单(共{}人):'.format(len(initial_names)))
-    for name in zip(initial_names):
+    # read initial name from file.
+    with open(f, 'r') as file:
+        content = file.readlines()
+    # you may also want to remove whitespace characters like `\n` at the end of each line
+    initial_names = [x.strip() for x in content]
+
+    print('============ 初始名单(共{}人) ============ '.format(len(initial_names)))
+    for name in initial_names:
         print(name)
 
     if t == 1:
@@ -79,7 +82,7 @@ def main(t, f):
         random_orders = randomOrder(len(random_names))
         file_name, ext = os.path.splitext(f)
         output_file = file_name + '_result' + ext
-        output_to_csv(output_file, random_names, random_orders)
+        output(output_file, random_names, random_orders)
     else:
         print('我不懂你想要干什么')
 
